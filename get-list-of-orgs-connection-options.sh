03b8a1978406eac30cb52950da097a66ee2ca83a
Jiri Hnidek <jhnidek@redhat.com>
2022-02-01 12:44:42 +0100

    Modified one example of GetOrgs() and add one more example of GetOrgs().

--- get-list-of-orgs-connection-options.sh
+++ get-list-of-orgs-connection-options.sh
@@ -2,6 +2,11 @@
 
 source ./library.sh
 
+# Note: magoo is not member of any organization
+export host="localhost"
+export port="8443"
+export handler="/candlepin"
+
 start_register_server
 
 echo "Trying to get list of organizations using dbus-send..."
@@ -9,7 +14,7 @@ dbus-send --address=${my_addr} --print-reply --dest='com.redhat.RHSM1.Register' 
        '/com/redhat/RHSM1/Register' \
        com.redhat.RHSM1.Register.GetOrgs string:"admin" \
        string:"admin" \
-       dict:string:string:"","" \
+       dict:string:string:"host","${host}","port","${port}","handler","${handler}" \
        string:""
 
 

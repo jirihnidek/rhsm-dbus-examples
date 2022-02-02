8ebbe96f4a8fdeccdc554e0f009b9e05c619b6ba
Jiri Hnidek <jhnidek@redhat.com>
2022-02-02 15:39:38 +0100

    Use more generic activation key.

--- register-org-activation-keys.sh
+++ register-org-activation-keys.sh
@@ -2,11 +2,8 @@
 
 source ./library.sh
 
-# NOTE: it will be probably necessary to change following organization_id and
-# activation key
-
 export org_id="admin"
-export activation_key="admin-awesomeos-all-x86-cont-key-ff8080817cdb07ac017cdb07ecfa0345"
+export activation_key="awesome_os_pool"
 
 start_register_server
 

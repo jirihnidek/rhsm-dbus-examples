aeb80341158e4f4c0d8273b68b806a86f41ef457
Jiri Hnidek <jhnidek@redhat.com>
2022-07-01 12:15:31 +0200

    Added more testing scripts.

---
+++ register-org-bad-username-password-de-locale.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ./library.sh
+
+export username="wrong"
+export password="wrong"
+export org_id="admin"
+
+start_register_server
+
+# Try to register using username and password
+echo "Registering using org, username and password..."
+dbus-send --address=${my_addr} --print-reply --dest='com.redhat.RHSM1.Register' \
+        '/com/redhat/RHSM1/Register' \
+        com.redhat.RHSM1.Register.Register \
+        string:"${org_id}" \
+        string:"${username}" \
+        string:"${password}" \
+        dict:string:string:"","" \
+        dict:string:string:"","" \
+        string:"de_DE.UTF-8" | gawk '/string/{ $1 = ""; print $0; }' | sed 's/\"//' | sed 's/\(.*\)\"/\1/' > /root/register_output.json
+
+print_registration_result $?
+
+stop_register_server
+
+unregister

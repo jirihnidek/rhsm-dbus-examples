881c03f007f7c460fec2c918da4fc1f061d4faf2
Jiri Hnidek <jhnidek@redhat.com>
2022-11-03 10:27:32 +0100

    New script for testing force registration and activation keys.

---
+++ register-org-activation-keys-force.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./library.sh
+
+export org_id="admin"
+export activation_key="awesome_os_pool"
+
+start_register_server
+
+# Try to register using activation key and org
+echo "Registering using activation key and org..."
+dbus-send --address=${my_addr} --print-reply --dest='com.redhat.RHSM1.Register' \
+       '/com/redhat/RHSM1/Register' \
+       com.redhat.RHSM1.Register.RegisterWithActivationKeys \
+       string:"${org_id}" \
+       array:string:"${activation_key}" \
+       dict:string:string:"force","1" \
+       dict:string:string:"","" \
+       string:"" | gawk '/string/{ $1 = ""; print $0; }' | sed 's/\"//' | sed 's/\(.*\)\"/\1/' > /root/register_output.json
+
+print_registration_result $?
+
+stop_register_server
+
+#unregister
